2f94da0e74151ab67d3a4cd5c4e08de31656e38b
xiw483 <55997619+xiw483@users.noreply.github.com>
2023-01-28 19:42:28 -0500

    update get_tweets

--- functions.py
+++ functions.py
@@ -1,23 +1,20 @@
-def get_tweets(handle, count=200):
-    # Authenticate with the Twitter API
-    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-    api = tweepy.API(auth)
-    
-    # Get the tweets
-    tweets = api.user_timeline(screen_name=handle, count=200)
-    
-    # Create a list to store the tweets
-    tweet_list = []
-    for tweet in tweets:
-        tweet_list.append(tweet)
+import tweepy
+
+class PoliticalSentiment:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
         
-    # Keep getting tweets until there are no more to get
-    while len(tweets) > 0:
-    # The max_id param will get the oldest tweets not yet retrieved
-    oldest_tweet = tweets[-1].id - 1
-    tweets = api.user_timeline(screen_name=handle, count=200, max_id=oldest_tweet)
-    for tweet in tweets:
-        tweet_list.append(tweet)
-        
-    return tweet_list
-    
+    def get_tweets(self, handle, count=200):
+        # Authenticate with the Twitter API
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        api = tweepy.API(auth)
+
+        # Get the tweets
+        tweets = api.user_timeline(screen_name=handle, count=count)
+
+        return tweets
+
